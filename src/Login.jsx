902da86300b5546aa79462c5c609c1f768c169fb
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-07-07 14:26:44 +0300

    accessToken

--- src/Login.jsx
+++ src/Login.jsx
@@ -36,6 +36,9 @@ const Login = () => {
       );
       console.log(JSON.stringify(response?.data));
       // console.log(JSON.stringify(response));
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+      setAuth({ user, pwd, roles, accessToken });
       setUser("");
       setPwd("");
       setSuccess(true);
